a697c150d441a822c1d9fd2b9077b01efa1366e3
UnnatiVe <unnativerma20022003@gmail.com>
2025-08-21 23:38:51 +0530

    Day 2

--- backend/controller/company.controller.js
+++ backend/controller/company.controller.js
@@ -35,13 +35,17 @@ export const registerCompany = async(req,res) =>{
 export const getCompany = async (req,res) =>{
     try{
       const userId = req.id;
-      const companies =   await Company.find({userId});
+      const companies =  await Company.find({userId});
       if(!companies){
         return res.status(404).json({
             message:"Companies not found.",
             success:false
         })
       }
+      return res.status(200).json({
+        companies,
+        success:true
+      })
     }
     catch(error){
         console.log(error);
